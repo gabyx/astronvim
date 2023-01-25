80480ca41ea4e877e98011f75bea37b2f30c4c0c
siduck <siduck@tutanota.com>
2023-01-25 14:31:21 +0530

    use lazy.nvim syntax for plugins

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,46 +1,45 @@
-local overrides = require "custom.plugins.overrides"
+local overrides = require("custom.plugins.overrides")
 
 return {
 
-  -- ["goolord/alpha-nvim"] = { disable = false } -- enables dashboard
+	-- Override plugin definition options
 
-  -- Override plugin definition options
-  ["neovim/nvim-lspconfig"] = {
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.plugins.lspconfig"
-    end,
-  },
+	["neovim/nvim-lspconfig"] = {
+		dependencies = {
+			-- format & linting
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require("custom.plugins.null-ls")
+			end,
+		},
 
-  -- overrde plugin configs
-  ["nvim-treesitter/nvim-treesitter"] = {
-    override_options = overrides.treesitter,
-  },
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.plugins.lspconfig")
+		end,
+	},
 
-  ["williamboman/mason.nvim"] = {
-    override_options = overrides.mason,
-  },
+	-- overrde plugin configs
+	["nvim-treesitter/nvim-treesitter"] = {
+		override_options = overrides.treesitter,
+	},
 
-  ["nvim-tree/nvim-tree.lua"] = {
-    override_options = overrides.nvimtree,
-  },
+	["williamboman/mason.nvim"] = {
+		override_options = overrides.mason,
+	},
 
-  -- Install a plugin
-  ["max397574/better-escape.nvim"] = {
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	["nvim-tree/nvim-tree.lua"] = {
+		override_options = overrides.nvimtree,
+	},
 
-  -- code formatting, linting etc
-  ["jose-elias-alvarez/null-ls.nvim"] = {
-    after = "nvim-lspconfig",
-    config = function()
-      require "custom.plugins.null-ls"
-    end,
-  },
+	-- Install a plugin
+	["max397574/better-escape.nvim"] = {
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
 
-  -- remove plugin
-  -- ["hrsh7th/cmp-path"] = false,
+	-- remove plugin
+	-- ["hrsh7th/cmp-path"] = false,
 }
