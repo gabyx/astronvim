ca91e1d4089dfab74341611ba0afaeb815c8663e
Pham Huy Hoang <hoangtun0810@gmail.com>
2023-02-23 18:51:01 +0700

    formatting issues

--- plugins.lua
+++ plugins.lua
@@ -3,52 +3,52 @@ local overrides = require("custom.configs.overrides")
 ---@type NvPluginSpec[]
 local plugins = {
 
-	-- Override plugin definition options
+  -- Override plugin definition options
 
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			-- format & linting
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
       {
         "jose-elias-alvarez/null-ls.nvim",
         config = function()
           require("custom.configs.null-ls")
         end,
       },
-		},
+    },
 
-		config = function()
-			require("plugins.configs.lspconfig")
-			require("custom.configs.lspconfig")
-		end,
-	},
+    config = function()
+      require("plugins.configs.lspconfig")
+      require("custom.configs.lspconfig")
+    end,
+  },
 
-	-- overrde plugin configs
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = overrides.treesitter,
-	},
+  -- overrde plugin configs
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = overrides.treesitter,
+  },
 
-	{
-		"williamboman/mason.nvim",
-		opts = overrides.mason,
-	},
+  {
+    "williamboman/mason.nvim",
+    opts = overrides.mason,
+  },
 
-	{
-		"nvim-tree/nvim-tree.lua",
-		opts = overrides.nvimtree,
-	},
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = overrides.nvimtree,
+  },
 
-	-- Install a plugin
-	{
-		"max397574/better-escape.nvim",
-		event = "InsertEnter",
-		config = function()
-			require("better_escape").setup()
-		end,
-	},
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
 
-	-- To make a plugin not be loaded
+  -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
   --   enabled = false
