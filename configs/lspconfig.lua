77673129b98a212099f55de142947744a7ec6981
Nuetzi, Gabriel <gabriel.nuetzi@dentsplysirona.com>
2023-03-03 13:27:31 +0100

    fix: Add pylsp

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -3,7 +3,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
 
-local servers = { "html", "cssls", "tsserver", "clangd", "rust_analyzer", "cmake", "golangci_lint_ls" }
+local servers = { "html", "cssls", "tsserver", "clangd", "rust_analyzer", "cmake", "golangci_lint_ls", "pylsp" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
