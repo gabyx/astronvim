cffb7989a3fcefe6aea035129149f980979467f5
Nuetzi, Gabriel <gabriel.nuetzi@dentsplysirona.com>
2023-03-07 07:52:33 +0100

    feat: Update to `v3.0`

--- init.lua
+++ init.lua
@@ -5,28 +5,25 @@
 -- normal format is "key = value". These also handle array like data structures
 -- where a value with no key simply has an implicit numeric key
 local config = {
-
     -- Configure AstroNvim updates
     updater = {
         remote = "origin", -- remote to use
         channel = "nightly", -- "stable" or "nightly"
         version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-        branch = "main", -- branch name (NIGHTLY ONLY)
-        commit = nil, -- commit hash (NIGHTLY ONLY)
+        branch = "main",   -- branch name (NIGHTLY ONLY)
+        commit = nil,      -- commit hash (NIGHTLY ONLY)
         pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
         skip_prompts = false, -- skip prompts about breaking changes
         show_changelog = true, -- show the changelog after performing an update
-        auto_quit = false -- automatically quit the current session after a successful update
+        auto_quit = false, -- automatically quit the current session after a successful update
         -- remotes = { -- easily add new remotes to track
         --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
         --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
         --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
         -- },
     },
-
     -- Set colorscheme to use
     colorscheme = "monokai",
-
     -- Add highlight groups in any theme
     highlights = {
         -- init = { -- this table overrides highlights in all themes
@@ -36,26 +33,25 @@ local config = {
         --   Normal = { bg = "#000000" },
         -- },
     },
-
     -- set vim options here (vim.<first_key>.<second_key> = value)
     options = {
         opt = {
             -- set to true or false etc.
             relativenumber = true, -- sets vim.opt.relativenumber
-            number = true, -- sets vim.opt.number
-            spell = false, -- sets vim.opt.spell
+            number = true,   -- sets vim.opt.number
+            spell = false,   -- sets vim.opt.spell
             signcolumn = "auto", -- sets vim.opt.signcolumn to auto
-            wrap = false -- sets vim.opt.wrap
+            wrap = false,    -- sets vim.opt.wrap
         },
         g = {
-            mapleader = " ", -- sets vim.g.mapleader
+            mapleader = " ",           -- sets vim.g.mapleader
             autoformat_enabled = true, -- enable or disable auto formatting at start (lsp.formatting.format_on_save must be enabled)
-            cmp_enabled = true, -- enable completion at start
-            autopairs_enabled = true, -- enable autopairs at start
-            diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
-            icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
-            ui_notifications_enabled = true -- disable notifications when toggling UI elements
-        }
+            cmp_enabled = true,        -- enable completion at start
+            autopairs_enabled = true,  -- enable autopairs at start
+            diagnostics_mode = 3,      -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+            icons_enabled = true,      -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
+            ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+        },
     },
     -- If you need more control, you can use the function()...end notation
     -- options = function(local_vim)
@@ -68,8 +64,7 @@ local config = {
     -- end,
 
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {underline = true},
-
+    diagnostics = { underline = true },
     -- Extend LSP configuration
     lsp = {
         -- enable servers that you already have installed without mason
@@ -79,16 +74,19 @@ local config = {
             format_on_save = {
                 enabled = true, -- enable or disable format on save globally
                 allow_filetypes = { -- enable format on save for specified filetypes only
-                    "go", "python", "rust", "markdown"
+                    "go",
+                    "python",
+                    "rust",
+                    "markdown",
                 },
                 ignore_filetypes = { -- disable format on save for specified filetypes
                     -- "python",
-                }
+                },
             },
             disabled = { -- disable formatting capabilities for the listed language servers
                 -- "lua_ls",
             },
-            timeout_ms = 1000 -- default format timeout
+            timeout_ms = 1000, -- default format timeout
             -- filter = function(client) -- fully override the default formatting function
             --   return true
             -- end
@@ -97,7 +95,7 @@ local config = {
         mappings = {
             n = {
                 -- ["<leader>lf"] = false -- disable formatting keymap
-            }
+            },
         },
         -- add to the global LSP on_attach function
         -- on_attach = function(client, bufnr)
@@ -128,9 +126,8 @@ local config = {
             --     },
             --   },
             -- },
-        }
+        },
     },
-
     -- Mapping data with "desc" stored directly by vim.keymap.set().
     --
     -- Please use this mappings table to set keyboard mapping since this is the
@@ -141,40 +138,43 @@ local config = {
         n = {
             -- second key is the lefthand side of the map
             -- mappings seen under group name "Buffer"
-            ["<leader>bb"] = {"<cmd>tabnew<cr>", desc = "New tab"},
+            ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New tab" },
             ["<leader>bc"] = {
                 "<cmd>BufferLinePickClose<cr>",
-                desc = "Pick to close"
+                desc = "Pick to close",
             },
-            ["<leader>bj"] = {"<cmd>BufferLinePick<cr>", desc = "Pick to jump"},
+            ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = "Pick to jump" },
             ["<leader>bt"] = {
                 "<cmd>BufferLineSortByTabs<cr>",
-                desc = "Sort by tabs"
+                desc = "Sort by tabs",
             },
             -- tables with the `name` key will be registered with which-key if it's installed
             -- this is useful for naming menus
-            ["<leader>b"] = {name = "Buffers"}
+            ["<leader>b"] = { name = "Buffers" },
         },
         t = {
             -- setting a mapping to false will disable it
             -- ["<esc>"] = false,
-        }
+        },
     },
-
     -- Configure require("lazy").setup() options
     lazy = {
-        defaults = {lazy = true},
+        defaults = { lazy = true },
         performance = {
             rtp = {
                 -- customize default disabled vim plugins
                 disabled_plugins = {
-                    "tohtml", "gzip", "matchit", "zipPlugin", "netrwPlugin",
-                    "tarPlugin", "matchparen"
-                }
-            }
-        }
+                    "tohtml",
+                    "gzip",
+                    "matchit",
+                    "zipPlugin",
+                    "netrwPlugin",
+                    "tarPlugin",
+                    "matchparen",
+                },
+            },
+        },
     },
-
     -- Configure plugins
     plugins = {
         {
@@ -184,29 +184,25 @@ local config = {
                 local monokai = require "monokai"
                 -- local palette = monokai.classic
                 monokai.setup {}
-            end
-        }, {
+            end,
+        },
+        {
             "simrat39/rust-tools.nvim",
             as = "rust-tools",
             config = function(_, opts)
-                local rt = require("rust-tools")
+                local rt = require "rust-tools"
                 opts = {}
-                rt.setup({
+                rt.setup {
                     server = {
                         on_attach = function(_, bufnr)
                             -- Hover actions
-                            vim.keymap.set("n", "<C-space>",
-                                           rt.hover_actions.hover_actions,
-                                           {buffer = bufnr})
+                            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
                             -- Code action groups
-                            vim.keymap.set("n", "<Leader>a",
-                                           rt.code_action_group
-                                               .code_action_group,
-                                           {buffer = bufnr})
-                        end
-                    }
-                })
-            end
+                            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+                        end,
+                    },
+                }
+            end,
         }, --
         -- You can disable default plugins as follows:
         -- { "max397574/better-escape.nvim", enabled = false },
@@ -290,11 +286,10 @@ local config = {
                     "███████║███████╗   ██║   ██████╔╝██║   ██║██║   ██║██║██╔████╔██║",
                     "██╔══██║╚════██║   ██║   ██╔══██╗██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║",
                     "██║  ██║███████║   ██║   ██║  ██║╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║",
-                    "╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝"
-
+                    "╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝",
                 }
                 return opts
-            end
+            end,
         }, --
         {
             "jose-elias-alvarez/null-ls.nvim",
@@ -321,30 +316,35 @@ local config = {
                     null_ls.builtins.formatting.markdownlint,
                     null_ls.builtins.formatting.markdown_toc,
                     null_ls.builtins.formatting.codespell,
-                    null_ls.builtins.formatting.stylua
+                    null_ls.builtins.formatting.stylua,
                 }
                 return config -- return final config table
-            end
-        }, --
+            end,
+        },            --
         -- Treesitter setup.
         {
             "nvim-treesitter/nvim-treesitter",
             opts = {
                 ensure_installed = {
-                    "lua", "rust", "toml", "python", "go", "cpp"
+                    "lua",
+                    "rust",
+                    "toml",
+                    "python",
+                    "go",
+                    "cpp",
                 },
                 auto_install = true,
                 highlight = {
                     enable = true,
-                    additional_vim_regex_highlighting = false
+                    additional_vim_regex_highlighting = false,
                 },
-                ident = {enable = true},
+                ident = { enable = true },
                 rainbow = {
                     enable = true,
                     extended_mode = true,
-                    max_file_lines = nil
-                }
-            }
+                    max_file_lines = nil,
+                },
+            },
         }, --
         -- Use mason-lspconfig to configure LSP installations
         {
@@ -352,10 +352,13 @@ local config = {
             -- overrides `require("mason-lspconfig").setup(...)`
             opts = {
                 ensure_installed = {
-                    "pyright", "rust_analyzer", "golangci_lint_ls", "clangd",
-                    "gopls"
-                }
-            }
+                    "pyright",
+                    "rust_analyzer",
+                    "golangci_lint_ls",
+                    "clangd",
+                    "gopls",
+                },
+            },
         }, --
         -- Use mason-null-ls to configure Formatters/Linter installation for null-ls sources
         {
@@ -363,27 +366,34 @@ local config = {
             -- overrides `require("mason-null-ls").setup(...)`
             opts = {
                 ensure_installed = {
-                    "prettier", "prettierd", "stylua", "rustfmt", "gofmt",
-                    "shfmt", "codespell", "markdownlint", "clang_format"
+                    "prettier",
+                    "prettierd",
+                    "stylua",
+                    "rustfmt",
+                    "gofmt",
+                    "shfmt",
+                    "codespell",
+                    "markdownlint",
+                    "clang_format",
                 },
                 automatic_installation = false,
-                automatic_setup = true -- Recommended, but optional
-            }
-        }, --
+                automatic_setup = true, -- Recommended, but optional
+            },
+        },                      --
         -- Use mason-nvim-dap to configure debug adapters.
         {
             "jay-babu/mason-nvim-dap.nvim",
             -- overrides `require("mason-nvim-dap").setup(...)`
             opts = {
-                ensure_installed = {"python", "delve", "codelldb"},
-                automatic_setup = true
-            }
+                ensure_installed = { "python", "delve", "codelldb" },
+                automatic_setup = true,
+            },
         }, --
-        -- Customize Source Priority
+        -- Customize source priority
         {
             "hrsh7th/nvim-cmp",
             dependencies = {
-                "hrsh7th/cmp-emoji" -- add cmp source as dependency of cmp
+                "hrsh7th/cmp-emoji", -- add cmp source as dependency of cmp
             },
             -- override the options table that is used in the `require("cmp").setup()` call
             opts = function(_, opts)
@@ -392,30 +402,43 @@ local config = {
                 local cmp = require "cmp"
                 -- modify the sources part of the options table
                 opts.sources = cmp.config.sources {
-                    {name = "nvim_lsp", priority = 1000},
-                    {name = "luasnip", priority = 750},
-                    {name = "buffer", priority = 500},
-                    {name = "path", priority = 250},
-                    {name = "emoji", priority = 700} -- add new source
+                    { name = "nvim_lsp", priority = 1000 },
+                    { name = "luasnip",  priority = 750 },
+                    { name = "buffer",   priority = 500 },
+                    { name = "path",     priority = 250 },
+                    { name = "emoji",    priority = 700 }, -- add new source
                 }
                 return opts
-            end
+            end,
         }, --
+        -- Use the awesome refactoring library.
+        {
+            "ThePrimeagen/refactoring.nvim",
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+                "nvim-treesitter/nvim-treesitter",
+            },
+            config = function(_, opts)
+            end,
+        }, --
+        -- Use the python refactoring library
+        { "python-rope/ropevim", run = "pip install ropevim", disable = false },
         -- Customize status bar
         {
             "rebelot/heirline.nvim",
             opts = function(_, opts)
-                local status = require("astronvim.utils.status")
-                opts.statusline = { -- statusline
-                    hl = {fg = "fg", bg = "bg"},
+                local status = require "astronvim.utils.status"
+                opts.statusline = {
+                    -- statusline
+                    hl = { fg = "fg", bg = "bg" },
                     status.component.mode {
-                        mode_text = {padding = {left = 1, right = 1}}
+                        mode_text = { padding = { left = 1, right = 1 } },
                     }, -- add the mode text
                     status.component.git_branch(),
                     status.component.file_info {
                         filetype = {},
                         filename = false,
-                        file_modified = false
+                        file_modified = false,
                     },
                     status.component.git_diff(),
                     status.component.diagnostics(),
@@ -424,13 +447,13 @@ local config = {
                     status.component.fill(),
                     status.component.lsp(),
                     status.component.treesitter(),
-                    status.component.nav()
+                    status.component.nav(),
                     -- remove the 2nd mode indicator on the right
                 }
 
                 -- return the final configuration table
                 return opts
-            end
+            end,
         }, --
         -- Adjust Icons to VS Code
         {
@@ -448,13 +471,12 @@ local config = {
                     Number = "",
                     Object = "",
                     Package = "",
-                    String = ""
+                    String = "",
                 }
                 return opts
-            end
-        }
+            end,
+        },
     },
-
     icons = {
         ActiveLSP = "",
         ActiveTS = " ",
@@ -497,9 +519,8 @@ local config = {
         Paste = "",
         Search = "",
         Selected = "",
-        TabClose = ""
+        TabClose = "",
     },
-
     -- This function is run last and is a good place to configuring
     -- augroups/autocommands and custom filetypes also this just pure lua so
     -- anything that doesn't fit in the normal config locations above can go here
@@ -526,8 +547,19 @@ local config = {
         -- vim.diagnostic.config({virtual_text = false})
         -- vim.diagnostic.config({signs = false})
 
+        -- load refactoring Telescope extension
+        require("telescope").load_extension "refactoring"
+
+        -- remap to open the Telescope refactoring menu in visual mode
+        vim.api.nvim_set_keymap(
+            "v",
+            "<leader>rr",
+            "<Esc><cmd>lua require('telescope').extensions.refactoring.refactors()<CR>",
+            { noremap = true }
+        )
+
         -- require("mason-nvim-dap").setup_handlers {}
-    end
+    end,
 }
 
 return config
