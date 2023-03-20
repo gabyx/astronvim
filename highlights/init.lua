83d54d95d9362e235470c1ab04e6478f6071162c
Nuetzi, Gabriel <gabriel.nuetzi@dentsplysirona.com>
2023-03-20 19:33:39 +0100

    fix: Correct misuse of highlights

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,9 +1,4 @@
 -- Add highlight groups in any theme
 return {
-  init = { -- this table overrides highlights in all themes
-    Visual = { bg = "#AAAAAA" },
-  },
-  duskfox = { -- a table of overrides/changes to the duskfox theme
-    Visual = { bg = "#AAAAAA" },
-  },
+  Visual = { bg = "#AAAAAA" },
 }
