9f659caeea571b33a5a9259a4759362955f4d54d
Nuetzi, Gabriel <gabriel.nuetzi@dentsplysirona.com>
2023-04-04 09:19:58 +0200

    fix: Adjust search git files

---
+++ plugins/telescope.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { -- add a new dependency to telescope that is our new plugin
+      "nvim-telescope/telescope-media-files.nvim",
+      -- "ahmedkhalf/project.nvim", -- defined in  ./editor.lua
+    },
+
+    -- The first parameter is the plugin specification
+    -- The second is the table of options as set up in Lazy with the `opts` key
+    opts = function(plugin, opts)
+      local telescope = require "telescope"
+      return require("astronvim.utils").extend_tbl(opts, {
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+          git_files = {
+            recurse_submodules = true,
+          },
+          buffers = {
+            path_display = { "smart" },
+          },
+        },
+      })
+    end,
+
+    config = function(plugin, opts)
+      require "plugins.configs.telescope"(plugins, opts)
+      local telescope = require "telescope"
+
+      -- telescope.load_extension "projects"
+      telescope.load_extension "media_files"
+    end,
+  },
+}
