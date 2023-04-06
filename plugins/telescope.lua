b82f236086264e7a33ed73ca4cbcaadc23e9c6cb
Nuetzi, Gabriel <gabriel.nuetzi@dentsplysirona.com>
2023-04-06 09:54:41 +0200

    fix: Correct quicklist mapping in telescope

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -10,7 +10,20 @@ return {
     -- The second is the table of options as set up in Lazy with the `opts` key
     opts = function(plugin, opts)
       local telescope = require "telescope"
+      local actions = require "telescope.actions"
+
       return require("astronvim.utils").extend_tbl(opts, {
+        defaults = {
+          mappings = {
+            -- Default mapping does not work for me `<M-q>`.
+            i = {
+              ["<C-f>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            },
+            n = {
+              ["<C-f>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            },
+          },
+        },
         pickers = {
           find_files = {
             hidden = true,
