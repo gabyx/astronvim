a2887ef293a425297241e931d03a163c3899cd79
Nuetzi, Gabriel <gabriel.nuetzi@dentsplysirona.com>
2023-05-08 17:42:47 +0200

    feat: Add tmux navigation

--- plugins/user.lua
+++ plugins/user.lua
@@ -23,7 +23,18 @@ return {
     lazy = false,
   },
   {
-    "christoomey/vim-tmux-navigator",
+    "alexghergh/nvim-tmux-navigation",
     lazy = false,
+    opts = {
+      disable_when_zoomed = true,
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+        last_active = "<C-\\>",
+        next = "<C-Space>",
+      },
+    },
   },
 }
