e1b227a062cb32b305bb0c58b329c19a86443fd1
Nuetzi, Gabriel <gabriel.nuetzi@dentsplysirona.com>
2023-05-17 14:33:34 +0200

    fix: Correct custom setup pyright LSP

--- init.lua
+++ init.lua
@@ -101,7 +101,6 @@ local config = {
 
     -- Load autocmds
     require "user.autocmds"
-
     -- Load refactoring Telescope extension
     require("telescope").load_extension "refactoring"
   end,
--- lsp/setup_handlers.lua
+++ lsp/setup_handlers.lua
@@ -1,3 +1,20 @@
 return { -- add custom handler
-  clangd = function(_, opts) require("clangd_extensions").setup { server = opts } end,
-},
+  clangd = function(server, opts) require("clangd_extensions").setup { server = opts } end,
+  pyright = function(server, opts)
+    -- Exclude some files for the root marker.
+    -- No `requirements.txt`
+    local root_files = {
+      "pyproject.toml",
+      "setup.py",
+      "setup.cfg",
+      "pyrightconfig.json",
+    }
+
+    local util = require "lspconfig.util"
+    local lspconfig = require "lspconfig"
+
+    lspconfig[server].setup(vim.tbl_deep_extend("force", opts, {
+      root_dir = util.root_pattern(unpack(root_files)),
+    }))
+  end,
+}
