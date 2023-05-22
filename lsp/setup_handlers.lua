ccdf4df1dc70388513f4610d699cc301820489bf
Nuetzi, Gabriel <gabriel.nuetzi@dentsplysirona.com>
2023-05-22 10:16:37 +0200

    fix: LSP setup for python

--- lsp/setup_handlers.lua
+++ lsp/setup_handlers.lua
@@ -1,10 +1,19 @@
 return { -- add custom handler
   clangd = function(server, opts) require("clangd_extensions").setup { server = opts } end,
   pyright = function(server, opts)
+    print(vim.inspect(opts))
     -- Exclude some files for the root marker.
     -- No `requirements.txt`
+    local root_files = {
+      "pyproject.toml",
+      "setup.py",
+      "setup.cfg",
+      "pyrightconfig.json",
+    }
+
     local util = require "lspconfig.util"
-    opts.root_dir = util.root_pattern("pyproject.toml", "setup.py", "setup.cfg", "pyrightconfig.json")
-    return opts
+    local lspconfig = require "lspconfig"
+    opts.root_dir = util.root_pattern(unpack(root_files))
+    lspconfig[server].setup(opts)
   end,
 }
