f53fd0a135e23e909be9538ced54b0731d68c1a7
Gabriel Nützi <gnuetzi@gmail.com>
2023-06-16 09:46:54 +0200

    fix: Add mardown preview plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -37,4 +37,10 @@ return {
       },
     },
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    cmd = { "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
 }
