4025ba1ea77d8f68f8ee217b03c7b1ac3774f6f0
Gabriel Nützi <gabriel.nuetzi@sdsc.ethz.ch>
2024-10-08 08:24:20 +0200

    fix: disable bashls formatting

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -29,6 +29,7 @@ return {
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         "nixd",
+        "bashls",
       },
       timeout_ms = 1000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
